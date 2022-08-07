9d0a8815c4a4e7d2784dc2667ced38921b6d92ea
Evan Payne <evan.payne@samtec.com>
2022-08-07 12:21:40 -0400

    first commit

---
+++ ObjectDetection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using ObjectDetection.YoloParser;
+using ObjectDetection.DataStructures;
+using ObjectDetection;
+using Microsoft.ML;
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+string GetAbsolutePath(string relativePath)
+{
+    FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+    string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+    string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+    return fullPath;
+}
+
